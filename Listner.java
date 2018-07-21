d6ec24c324d2dc7caa51df82f3d72c1a87ed1354
psamantaray <prasantsamantaray222@gmail.com>
2018-07-21 14:00:50 +0530

    Seizing

--- Listner.java
+++ Listner.java
@@ -11,7 +11,7 @@ public class Listner extends TestRunner implements ITestListener {
 	@Override
 	public void onTestStart(ITestResult result) {
 		System.out.println("Test case Execution Started!");
-	System.out.println("Started...");
+           	System.out.println("Started...");
 	}
 
 	@Override
